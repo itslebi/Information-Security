36642d0a0a2f52c84004d8e905d9ff9fae0eae5d
Le Bi <lebidarra@gmail.com>
2024-08-06 14:58:35 +0000

    Changes to be committed: modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const helmet = require("helmet");
+//app.use(helmet.hidePoweredBy());
 app.use(helmet.hidePoweredBy());
-
-
+app.use(helmet.frameguard({action: 'deny'}));
 
 
 
